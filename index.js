a1e35228d851c8ea537434a518dbd54264300d0e
AyushPanday1 <ayush7426883261@gmail.com>
2022-12-08 18:20:43 +0530

    Done

--- index.js
+++ index.js
@@ -1,21 +1,19 @@
 const express = require('express')
 const mongoose = require('mongoose')
 const app = express()
+app.use(express.json());
+
 
 const route = require('./route/route')
 
-app.use(express.json())
-
-mongoose.set('strictQuery', false);
-
-mongoose.connect("mongodb+srv://AyushPanday:AyushPan123@cluster0.eixapeq.mongodb.net/test",{
+mongoose.connect("mongodb+srv://insh007:Inshad123@firstcluster.p0r04o1.mongodb.net/group41Database",{
     useNewUrlParser : true
 })
 .then(() => console.log("MongoDB is connected"))
 .catch(err => console.log(err))
 
-app.use('/',route)
+app.use(route)  
 
-app.listen(3000 , function(){
-    console.log("Running on the port " , 3000);
+app.listen(3000, function(){
+    console.log("Express app is running on port " + 3000)
 })
